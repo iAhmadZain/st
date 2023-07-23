a87d18acd292b97a6f1d77f13092df388defd1ce
Ahmad Zain <iahmadzain@gmail.com>
2023-07-24 00:28:16 +0300

    - Adding use case for Octane concruncy - Update composer

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -5,14 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Cart extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'name',
-        'description',
-        'price',
-        'category_id',
+        'user_id',
+        'product_id',
+        'quantity',
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Models\Product;
+use App\Models\Review;
+use App\Models\Cart;
+use Illuminate\Http\Request;
+use Laravel\Octane\Facades\Octane;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +19,23 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/product/{id}', function (Request $request, $id) {
+    $productDetails = Octane::concurrently([
+        'product' => function () use ($id) {
+            return Product::find($id);
+        },
+        'reviews' => function () use ($id) {
+            return Review::where('product_id', $id)->get();
+        },
+        'relatedProducts' => function () use ($id) {
+            // Fetch related products based on the product category or other criteria
+            return Product::where('category_id', $id)->get();
+        },
+        'cart' => function () use ($request) {
+            // Fetch the current user's shopping cart
+            return Cart::where('user_id', $request->user()->id)->get();
+        },
+    ]);
 
-Route::get('/product/{id}', [ProductController::class, 'show']);
+    return view('product', $productDetails);
+});
